5d8270119384832194f7aaaa33e8b56f74509874
Rahul Routh <120283349+rahulrajputt@users.noreply.github.com>
2024-11-25 22:46:20 +0530

    update

---
+++ wp-content/wp-defender-secrets.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The file is used to generate and validate secret keys. Deleting this file or manually editing the content will
+ * permanently break 2fa TOTP method for all users.
+ *
+ * @package WP_Defender
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+// WP Defender - Start.
+define( 'WP_DEFENDER_TOTP_KEY', 'woupVZrM8t2JV/jK11R4K2cCkpSS3yafAPkhowH02Dk=' );
+// WP Defender - End.
